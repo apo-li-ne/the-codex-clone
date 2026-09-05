1066d560eef31d4455ee42e34fe505bcc841a956
apolguil <apolguil@f1br1s1.paris.42.school>
2026-09-06 00:17:55 +0200

    htuerhgiuer

---
+++ ft_alembic_5.py
@@ -0,0 +1,5 @@
+import alchemy.elements
+
+print("=== Alembic 5 ===")
+print("Accessing the alchemy module using 'from alchemy import ...'")
+print(f"Testing create_air: {alchemy.elements.create_air()}")
